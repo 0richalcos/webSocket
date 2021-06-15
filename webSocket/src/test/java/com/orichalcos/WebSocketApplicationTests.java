571343e09b5cdbc97b90c66a513e349f6c074afc
0richalcos <xox.zhe@foxmail.com>
2021-06-15 15:19:55 +0800

    init

---
+++ webSocket/src/test/java/com/orichalcos/WebSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orichalcos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
